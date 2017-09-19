4a0e164079ab832a1760bd1d4e93c4d503043fbb
iagoslc <32021308+iagoslc@users.noreply.github.com>
2017-09-19 18:31:21 -0300

    Add files via upload

--- Scripts/make_sac2pkl.py
+++ Scripts/make_sac2pkl.py
@@ -1,13 +1,27 @@
 # programa para criar os arquivos .pkl 
 # Iago Costa
 
+import sys, os
 from os import listdir
+from obspy import read
 
 files = listdir(".")
+outfile='processing_files.sh'
 
-f = open('processing_files', 'w')
+
+
+#normalizacao dos dados para auxilio na análise do MCPCC considerando que nao serao feitas
+#correcoes de amplitude
+
+for lines in listdir("."):
+		st=read(lines)
+		tr = st[0]
+		tr_norm=tr.normalize()
+		tr_norm.write(filename=lines+'n',format="SAC")
 
 prefix_tables=[]
+
+
 
 for lines in files:
 	line_prefix=lines[:14]
@@ -16,12 +30,20 @@ for lines in files:
 
 teste = len(prefix_tables) 
 
+f = open(outfile, 'w')
+
 c = 0
 
 for var in prefix_tables:
 	c=c+1
 	print c
-	print('ttpick.py -P %s*\n' % var)
-	f.write('ttpick.py -P %s*\n' % var)
+	print('processando %s* \n' % (var))
+	f.write('sac2pkl.py -s %s* -o %s.pkl -d 0.025\n' % (var,var))
+
 
 f.close()
+
+os.system(outfile)
+
+os.remove(outfile)
+
